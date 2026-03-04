756def95f8efb5a43239f5ab8b370dd2b9012f61
woz5999 <jeff.wozniak@umbra.space>
2026-03-04 14:44:51 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,6 +7,8 @@ PORT="${2:-8888}"
 docker build -t "$IMAGE_NAME" .
 echo "Building Docker image '$IMAGE_NAME' completed."
 
+docker stop "$IMAGE_NAME" 2>/dev/null || true
+doker rm "$IMAGE_NAME" 2>/dev/null || true
 docker run --rm -d -p "$PORT":8888 --name "$IMAGE_NAME" \
   -v "$(pwd)":/workspace \
   "$IMAGE_NAME"
